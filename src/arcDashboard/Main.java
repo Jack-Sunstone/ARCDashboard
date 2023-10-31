7a2d5ca8fd203caab273614d7d740cc6756702c0
Jack-Sunstone <jack@sunstone-systems.com>
2023-10-31 11:39:25 +0000

    Created First Stage

--- src/arcDashboard/Main.java
+++ src/arcDashboard/Main.java
@@ -1,5 +1,21 @@
 package arcDashboard;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("My First JavaFX App");
+
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
